4c084a432a27d2fc0df5d86ea44557e1e4ed4a21
Roshan G. Bolonna <srgbolonna@gmail.com>
2025-01-12 16:08:01 +0530

    done: cors unrestricted, this is not ideal due to less secrure but for dev-testing this is required.

--- startup/server.go
+++ startup/server.go
@@ -10,7 +10,7 @@ import (
 // CORS middleware function
 func enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://html-parser-service:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
